7a6c283b2fafce45716eacd5fabdb311d177afd3
Your Name <you@example.com>
2021-04-08 14:50:33 +0530

    venkat

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from "react-scroll";
 import "./Header.css";
-const Header = () => {
+const Header = ({ selected, setSelected, setOpen }) => {
    return (
       <div className="header">
          <div className="header__left">
@@ -10,33 +10,54 @@ const Header = () => {
             </h1>
          </div>
          <div className="header__right">
-            <Link to="About" smooth={true} duration={100}>
-               <h4>
+            <Link to="About" smooth={true} duration={500}>
+               <h4
+                  className={
+                     selected ==="About" ? "header__right--active" : undefined
+                  }
+                  onclick={() => setSelected("About")}
+                  >
                   About me
                </h4>
             </Link>
-            <Link to="skills" smooth={true} duration={100}>
-               <h4>
+            <Link to="skills" smooth={true} duration={500}>
+               <h4
+               className={
+                  selected === "skills" ? "header__right--active" : undefined
+               }
+               onclick={() => setSelected("skills")}
+               >
                   skills
                </h4>
             </Link>
             <Link Link to="Projects" smooth={true} duration={100}>
-               <h4>
+               <h4
+               className={
+                  selected === "Projects" ? "header__right--active" : undefined
+               }
+               onclick={() => setSelected("Projects")}
+               >
                   Projects
                </h4>
                </Link>
             
-            <Link to="Contact" smooth={true} duration={100}>
-               <h4>
+            <Link to="Contact" smooth={true} duration={500}>
+               <h4
+               className={
+                  selected === "Contact" ? "header__right--active" : undefined
+               }
+               onclick={() => setSelected("Contact")}
+               >
                   Contact
                </h4>
             </Link>
-            <h4 className="header__rightButton">Join with me</h4>
-            
+            <h4 className="header__right--semiactive" onclick={() => setOpen(true)}>
+               Join with me
+               </h4>
          </div>
          
       </div>
-   )
-}
+   );
+};
 
 export default Header;
